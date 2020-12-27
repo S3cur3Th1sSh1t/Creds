afda9899e9db3371948f7fb0d96c67aac63f37e3
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2020-12-27 13:40:27 +0100

    Create CSharpToNimByteArray.ps1

---
+++ helpers/CSharpToNimByteArray.ps1
@@ -0,0 +1,40 @@
+function CSharpToNimByteArray
+{
+
+Param
+    (
+        [string]
+        $inputfile,
+	    [switch]
+        $folder
+)
+
+    if ($folder)
+    {
+        $Files = Get-Childitem -Path $inputfile -File
+        $fullname = $Files.FullName
+        foreach($file in $fullname)
+        {
+            Write-Host "Converting $file"
+            $outfile = $File + "NimByteArray.txt"
+    
+            [byte[]] $hex = get-content -encoding byte -path $File
+            $hexString = ($hex|ForEach-Object ToString X2) -join ',0x'
+            $Results = $hexString.Insert(0,"var buf: array[" + $hex.Length + ", byte] = [byte 0x")         
+            $Results | out-file $outfile
+         
+        }
+        Write-Host -ForegroundColor yellow "Results Written to the same folder"
+    }
+    else
+    {
+        Write-Host "Converting $inputfile"
+        $outfile = $inputfile + "NimByteArray.txt"
+        
+        [byte[]] $hex = get-content -encoding byte -path $inputfile
+        $hexString = ($hex|ForEach-Object ToString X2) -join ',0x'
+        $Results = $hexString.Insert(0,"var buf: array[" + $hex.Length + ", byte] = [byte 0x")         
+        $Results | out-file $outfile
+        Write-Host "Result Written to $outfile"
+    }
+} 
